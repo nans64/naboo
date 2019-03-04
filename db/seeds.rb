e924a9ebcb6bff0a3e396f14329f95e73f17cef8
dany-about <dany.about@edu.escpeurope.eu>
2019-03-04 17:22:44 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,26 @@
 Products.destroy_all
 
 images = [
-"https://sun6-2.userapi.com/c845421/v845421014/1b4fbc/CwUL3CGbF5U.jpg",
-"https://sun6-4.userapi.com/c845421/v845421014/1b4fc5/7Mj7kMkRoY4.jpg",
-"https://pp.userapi.com/c845421/v845421014/1b4fce/qDl9C_48Ulg.jpg",
-"https://sun6-2.userapi.com/c845421/v845421014/1b4fd7/EmVqwdTYywQ.jpg",
-"https://sun6-1.userapi.com/c845421/v845421014/1b4fe0/VMkpsELiu70.jpg",
-"https://sun6-3.userapi.com/c845421/v845421014/1b4fe9/A-UypHbf2YE.jpg",
-"https://sun6-1.userapi.com/c845421/v845421014/1b4ffb/DO9LhGfjh_4.jpg"
+  "https://jardinage.lemonde.fr/images/dossiers/2019-02/ural-rex-111519.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2019-02/mau-arabe-1-062135.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2019-02/californian-rex-155643.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2019-01/serengeti-161713.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2019-01/australian-mist-1-153708.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2019-01/bobtail-kouriles-1-165633.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-12/seychellois-1-114130.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-12/safari-120638.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-12/skookum-162403.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-12/minskin-chat-165744.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-11/mandarin-143656.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-11/york-chocolat-075558.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-11/sokoke-160801.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-11/ojos-azules-062745.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-10/anatoli-1-094710.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-10/colorpoint-shorthair-065619.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-10/ashera-1-153825.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-09/highland-fold-1-120133.jpg"
+  "https://jardinage.lemonde.fr/images/dossiers/2018-09/donskoy1-151329.jpg",
+  "https://jardinage.lemonde.fr/images/dossiers/2018-09/havana-brown-1-134550.jpg"
 ]
 
 titles = [
@@ -33,9 +46,14 @@ titles = [
   "Miskin",
   "Mandarine",
   "York-Choco",
-  ""
-
-
+  "Sokoké",
+  "Ojos-Azule",
+  "Anatole",
+  "Colorpoint",
+  "Ashera",
+  "Highlander",
+  "Dostoyevsky",
+  "Havana-na-na-na"
 ]
 
 descriptions = [
@@ -51,11 +69,16 @@ descriptions = [
   " est dépourvu de poils, même si quelquefois il arbore un léger duvet satiné ayant l’aspect du cachemire. Sa peau forme comme des plis.",
   " est un chat très élégant et svelte, caractéristiques qu’il tient du Siamois et de l’Oriental dont il descend. Ses pattes sont longues et musclées avec une ossature fine. Il possède une robe mi-longue caractérisée par des poils plus courts sur ses épaules qui s’allongent ensuite sur ses flancs, et surtout sa queue est en panache. Son poil, particulièrement soyeux, est couché sur son corps.",
   " est musclé mais tout en élégance. Ses yeux, de forme ovale, sont d’une couleur or ou verte intense et il possède deux oreilles aussi hautes que larges à leur base, bien dressées sur sa petite tête ronde.",
-
-
-
+  " a comme particularité de vivre dans les arbres. De taille moyenne, il possède un corps longiligne très musclé, de longues pattes ainsi que des pieds assez gros. Ses pattes de derrière sont un peu plus grandes que celles de devant. Sa tête est de forme triangulaire avec des joues et des pommettes proéminentes.",
+  " a une tête de forme triangulaire avec des pommettes saillantes et un museau carré. Ses oreilles sont de taille moyenne, un peu arrondies à leur extrémité et bien dressées sur la tête.",
+  " est un chat long et musclé, à la poitrine large et à l’ossature forte. Avec ses pattes rondes et sa queue épaisse de longueur moyenne, c’est un chat à l’allure massive. Sa tête est en forme de triangle réhaussée de deux grandes oreilles aux extrémités arrondies.",
+  " est un chat avec différentes caractéristiques bien particulières. Il possède d’abord une tête bien reconnaissable de forme triangulaire au-dessus de laquelle se dressent deux grandes oreilles. Son nez est long et droit et ses yeux en forme d’amande sont d’un bleu clair typique.",
+  " ressemble en de nombreux points à un petit léopard. Sa robe a en effet la même apparence, ainsi que son museau court et arrondi. C’est en plus un chat de très grande taille, aux mensurations presque démesurées, à l’allure majestueuse et élégante, qui peut même être promené en laisse.",
+  " est un chat très reconnaissable, tout en rondeur, qui ressemble beaucoup au Scottish fold, mais avec des poils plus longs. Sa robe est en effet mi-longue et soyeuse. Elle comporte un sous-poil très épais et ses poils forment une collerette au niveau de son cou. Son corps dégage de la puissance malgré son allure de peluche.",
+  " possède un corps très musclé qui dégage une grande puissance. Il est recouvert de plis bien présents et visibles sur son ventre, son encolure et ses aisselles. Ses pattes sont longues et lui donnent une allure très atypique, tout comme sa longue queue fine.",
+  " a une allure assez unique avec sa robe entièrement couleur chocolat, aux nuances d’acajou, lisse, douce et brillante. Cette dernière met en avant la couleur intense de ses grands yeux ovales, toujours verts."
 ]
 
-7.times do |i|
+20.times do |i|
   Products.create!(title: titles[i], description: title[i]+descriptions[i], price: rand(0..99)+0.99, image_url: images[i])
 end
